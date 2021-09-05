c5d00423732fd67523a6a38bcc5249e1bbeed08e
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-09-05 21:13:59 +0300

    Added random direction for ghosts

--- ghost_class.py
+++ ghost_class.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from settings import *
 
 vec = pygame.math.Vector2
@@ -21,14 +22,44 @@ class Ghost:
         if self.time_to_move():
             self.move()
 
+        # Grid position in reference to pixel position
+        self.grid_pos[0] = (self.pixel_pos[0] + self.app.cell_width // 2) // self.app.cell_width
+        self.grid_pos[1] = (self.pixel_pos[1] - TOP_BOTTOM_MARGIN + self.app.cell_height // 2) // self.app.cell_height + 1
+
     def draw(self):
-        pygame.draw.circle(self.app.screen, self.color, self.pixel_pos, self.radius)
+        pygame.draw.circle(self.app.screen, self.color, (int(self.pixel_pos.x + self.app.cell_width // 2), int(self.pixel_pos.y)), self.radius)
 
     def time_to_move(self):
-        pass
+        if self.pixel_pos.x % self.app.cell_width == 0:
+            if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
+                return True
+        if int(self.pixel_pos.y + TOP_BOTTOM_MARGIN // 2) % self.app.cell_height == 0:
+            if self.direction == vec(0, 1) or self.direction == vec(0, -1):
+                return True
+        else:
+            return False
 
     def move(self):
-        pass
+        # if self.personality == 'random':
+        #     self.direction = self.get_random_direction()
+        self.direction = self.get_random_direction()
+
+    def get_random_direction(self):
+        while True:
+            number = random.randint(-2, 1)
+            if number == -2:
+                x_dir, y_dir = 1, 0
+            elif number == -1:
+                x_dir, y_dir = 0, 1
+            elif number == 0:
+                x_dir, y_dir = -1, 0
+            else:
+                x_dir, y_dir = 0, -1
+            next_pos = vec(self.grid_pos.x + x_dir, self.grid_pos.y + y_dir)
+            if next_pos not in self.app.walls:
+                break
+        return vec(x_dir, y_dir)
+
 
     def get_pixel_pos(self):
         return vec(
