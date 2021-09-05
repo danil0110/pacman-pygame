e7b5022cc3af91c82722dba19475abdfa4234812
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-09-05 17:15:41 +0300

    Locked pacman's movement to grid

--- player_class.py
+++ player_class.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from settings import *
 
 vec = pygame.math.Vector2
@@ -10,16 +11,25 @@ class Player:
         self.grid_pos = pos
         self.pixel_pos = self.get_pixel_pos()
         self.direction = vec(1, 0)
+        self.stored_direction = None
 
     def update(self):
         self.pixel_pos += self.direction
+        if self.pixel_pos.x % self.app.cell_width == 0:
+            if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
+                if self.stored_direction is not None:
+                    self.direction = self.stored_direction
+        if int(self.pixel_pos.y + TOP_BOTTOM_MARGIN // 2) % self.app.cell_height == 0:
+            if self.direction == vec(0, 1) or self.direction == vec(0, -1):
+                if self.stored_direction is not None:
+                    self.direction = self.stored_direction
 
         # Grid position in reference to pixel position
         self.grid_pos[0] = (self.pixel_pos[0] + self.app.cell_width // 2) // self.app.cell_width
         self.grid_pos[1] = (self.pixel_pos[1] - TOP_BOTTOM_MARGIN + self.app.cell_height // 2) // self.app.cell_height + 1
 
     def draw(self):
-        pygame.draw.circle(self.app.screen, PLAYER_COLOR, (int(self.pixel_pos.x), int(self.pixel_pos.y)),
+        pygame.draw.circle(self.app.screen, PLAYER_COLOR, (int(self.pixel_pos.x + self.app.cell_width // 2), int(self.pixel_pos.y)),
                            self.app.cell_width // 2 - 2)
 
         # Grid position rectangle
@@ -28,9 +38,10 @@ class Player:
                                                 self.app.cell_height), 1)
 
     def move(self, direction):
-        self.direction = direction
+        self.stored_direction = direction
 
     def get_pixel_pos(self):
-        return vec((self.grid_pos.x * self.app.cell_width) + self.app.cell_width // 2,
-                   (self.grid_pos.y * self.app.cell_height) + TOP_BOTTOM_MARGIN // 2
-                   + self.app.cell_height // 2)
+        return vec(
+            (self.grid_pos.x * self.app.cell_width) + self.app.cell_width // 2,
+            (self.grid_pos.y * self.app.cell_height) + TOP_BOTTOM_MARGIN // 2 + self.app.cell_height // 2
+        )
