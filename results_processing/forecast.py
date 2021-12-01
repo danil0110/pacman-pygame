ea7424ad81b17dee329058aab84cfda06f557d1b
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-12-01 22:55:24 +0200

    Small improvement

--- results_processing/forecast.py
+++ results_processing/forecast.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
 predict_count = 5
+figure_size = 8
 dataset = pd.read_csv('game_stats.csv')[['is_win', 'score', 'time']]
 dataset, test_data = dataset.iloc[:-predict_count], dataset.iloc[-predict_count:]
 
@@ -19,8 +20,8 @@ fit_x, fit_y = pca.fit_transform(X, Y).T
 
 fit_lose_x, _ = pca.fit_transform(lose_dataset[x_col]).T
 
-figure = plt.figure(figsize=(10, 10))
-m2 = plt.scatter(fit_lose_x, lose_dataset.score, 10, "red")
+figure = plt.figure(figsize=(figure_size, figure_size))
+m2 = plt.scatter(fit_lose_x, lose_dataset.score, figure_size, "red")
 plt.show()
 
 model = linear_model.LinearRegression()
@@ -31,8 +32,8 @@ y_prediction_line_lose = model.predict(lose_dataset[x_col])
 
 fit_prediction_lose_X, fit_prediction_lose_Y = pca.fit_transform(lose_dataset[x_col], pd.DataFrame(y_prediction_line_lose)[0]).T
 
-figure = plt.figure(figsize=(10, 10))
-m2 = plt.scatter(fit_lose_x, lose_dataset.score, 10, color="red")
+figure = plt.figure(figsize=(figure_size, figure_size))
+m2 = plt.scatter(fit_lose_x, lose_dataset.score, figure_size, color="red")
 plt.plot(fit_prediction_lose_X, y_prediction_line_lose, color="red", linewidth=2, label="Prediction")
 plt.show()
 
